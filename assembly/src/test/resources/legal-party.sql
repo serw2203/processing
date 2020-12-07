2fd994c7c555d3dafeafea85093087725ef9cb03
serw2203 <s.v.barkhatov@gmail.com>
2020-12-07 19:04:10 +0300

    jpa

---
+++ assembly/src/test/resources/legal-party.sql
@@ -0,0 +1,1 @@
+insert into LEGAL_PARTY (id, name,INN,Account,segment,ogrn,IDCRM) values (PROCESSING_SEQ.nextval, 'OOO RMI-PRODUKT','4703059999','40702810202099998888','Крупные','1034700569876','1-CW8AA');
